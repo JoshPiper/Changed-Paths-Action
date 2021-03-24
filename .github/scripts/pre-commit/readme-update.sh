9a45dea5e3da5929e994466f42d57c5464a87b32
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-03-24 20:45:14 +0000

    fix: fixed readme updater.

--- .github/scripts/pre-commit/readme-update.sh
+++ .github/scripts/pre-commit/readme-update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-perl -pi -e "s/JoshPiper\/GModStore-Version-Parser\@([\w.]+)?/JoshPiper\/GModStore-Version-Parser\@$BUILD_TAG/g" README.md
+perl -pi -e "s/JoshPiper\/Changed-Paths-Action\@([\w.]+)?/JoshPiper\/Changed-Paths-Action\@$BUILD_TAG/g" README.md
