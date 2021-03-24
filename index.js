f27af37a1ff6f1a9e545dc5ec5f532a25a07f167
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-03-24 19:28:15 +0000

    why the fuck?

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ async function getLastWorkflowSHA(){
  * @returns {Promise<?string>}
  */
 async function getBranchDeviation(base = undefined, split = undefined){
-	core.info("base", base, master)
-	core.info("split", split, branch)
+	core.info("base", base, "default", master)
+	core.info("split", split, "default", branch)
 	if (base === undefined){
 		base = master
 	}
